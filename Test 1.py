e224b633da1882ba62bc864378e517842c82fc18
adavidkhowantolim <adavidkhowantolim@gmail.com>
2024-05-21 10:20:53 +0700

    remove unnecessary import

--- Test 1.py
+++ Test 1.py
@@ -1,24 +1,25 @@
-import sys
+def miniMaxSum():
+  # Input number list
+  numInput = input ("Enter number :") 
+  # initialize max and min values
+  minNum = pow(10,9)
+  maxNum = 0
 
-# Input number list
-numInput = input ("Enter number :") 
-# initialize max and min values
-minNum = pow(10,9)
-maxNum = 0
+  #### process input and getting the max & min sum of 4 of the 5 integers ####
+  # assuming input always five space-separated integers (from the problem description)
+  try:
+    # process input
+    numArr = numInput.strip().split(" ")
+    # convert list of string to list of int and sort them ascending
+    sortedNumArr = sorted([int(i) for i in numInput.strip().split(" ")])
+    # get max and min number
+    maxNum = sum(sortedNumArr[1:])
+    minNum = sum(sortedNumArr[:-1])
+  except ValueError as error:
+    print (error)
+  except:
+    print("Other Errors")
 
-#### process input and getting the max & min sum of 4 of the 5 integers ####
-# assuming input always five space-separated integers (from the problem description)
-try:
-  # process input
-  numArr = numInput.strip().split(" ")
-  # convert list of string to list of int and sort them ascending
-  sortedNumArr = sorted([int(i) for i in numInput.strip().split(" ")])
-  # get max and min number
-  maxNum = sum(sortedNumArr[1:])
-  minNum = sum(sortedNumArr[:-1])
-except ValueError as error:
-  print (error)
-except:
-  print("Other Errors")
+  print(minNum, maxNum)
 
-print(minNum, maxNum)
+miniMaxSum()
