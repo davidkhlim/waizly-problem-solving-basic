f158b80f2cec021b68450f6df2b7c3a9b32b90bc
adavidkhowantolim <adavidkhowantolim@gmail.com>
2024-05-21 09:43:54 +0700

    Test 1 Done

---
+++ Test 1.py
@@ -0,0 +1,24 @@
+import sys
+
+# Input number list
+numInput = input ("Enter number :") 
+# initialize max and min values
+minNum = pow(10,9)
+maxNum = 0
+
+#### process input and getting the max & min sum of 4 of the 5 integers ####
+# assuming input always five space-separated integers (from the problem description)
+try:
+  # process input
+  numArr = numInput.strip().split(" ")
+  # convert list of string to list of int and sort them ascending
+  sortedNumArr = sorted([int(i) for i in numInput.strip().split(" ")])
+  # get max and min number
+  maxNum = sum(sortedNumArr[1:])
+  minNum = sum(sortedNumArr[:-1])
+except ValueError as error:
+  print (error)
+except:
+  print("Other Errors")
+
+print(minNum, maxNum)
